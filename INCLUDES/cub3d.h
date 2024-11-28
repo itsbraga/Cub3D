651ebbda713f1a66392b57c7949fc17ff2748fbf
k0xdama <prioux.mateo@outlook.com>
2024-11-28 17:26:28 +0100

    directory structure + LIBFT

---
+++ INCLUDES/cub3d.h
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3d.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pmateo <pmateo@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/28 17:08:40 by pmateo            #+#    #+#             */
+/*   Updated: 2024/11/28 17:10:15 by pmateo           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUB3D_H
+# define CUB3D_H
+
+# include <unistd.h>
+# include <stdio.h>
+# include <stddef.h>
+# include <stdlib.h>
+# include <math.h>
+# include <X11/X.h>
+# include <X11/keysym.h>
+# include "../minilibx-linux/mlx.h"
+# include "../LIBFT/INCLUDES/libft.h"
+# include "../LIBFT/INCLUDES/ft_printf.h"
+
+
+
+
+
+
+
+
+#endif
